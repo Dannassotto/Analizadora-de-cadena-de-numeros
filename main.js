eeb01a4511b4d372825d0a218f0d8e654c45f454
Dannassotto <valentinaguerrero0810@gmail.com>
2024-09-17 09:44:50 -0500

    Analizadora de cadena de numeros

---
+++ main.js
@@ -0,0 +1,21 @@
+const inputNumeros = document.getElementById("inputNumeros");
+const resultadoTexto = document.getElementById("resultadoTexto");
+const resultadoNumeros = document.getElementById("resultadoNumeros");
+const botonEnviar = document.getElementById("botonEnviar");
+
+botonEnviar.addEventListener("click", () => {
+    const numeros = inputNumeros.value.split("").map(Number);
+    const resultadoHTML = [];
+
+    numeros.forEach((numero, indice) => {
+        const esPar = numero % 2 === 0;
+        const tamaño = esPar ? `font-size: ${indice + 10}px` : "";
+        const color = esPar ? "color: blue" : "";
+        const estilo = esPar ? `style="${tamaño}; ${color}"` : "";
+
+        resultadoHTML.push(`<span ${estilo}>${numero}</span>`);
+    });
+
+    resultadoNumeros.innerHTML = resultadoHTML.join("");
+});
+
